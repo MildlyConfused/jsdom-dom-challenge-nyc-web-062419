2eedf28b862f97e02ca5bbd14d9555fa7599099f
Ben Braunstein <benzbraunstein@gmail.com>
2019-08-07 11:44:09 -0400

    Done.

--- challenge.js
+++ challenge.js
@@ -1,8 +1,20 @@
+// Make a bunch of stuff
 let form = document.querySelector("#comment-form");
 const comment = text => `<li>${text}</li>`;
 const commentsList = document.querySelector("#list");
 const heartArray = [];
+const counter = document.querySelector("#counter");
+const buttonDiv = document.querySelector("#buttonDiv");
+const likeList = document.querySelector(".likes");
+// Bool to see if timer is running or not
+let isRunning = true;
+// Start running the timer
+let counterInterval = setInterval(function() {
+  isRunning = true;
+  changeTimer(1);
+}, 1000);
 
+// Form to add comments to comment list
 form.addEventListener("submit", function(e) {
   e.preventDefault();
   const term = document.querySelector("#search").value;
@@ -10,19 +22,7 @@ form.addEventListener("submit", function(e) {
   form.reset();
 });
 
-const counter = document.querySelector("#counter");
-
-const buttonDiv = document.querySelector("#buttonDiv");
-
-const likeList = document.querySelector(".likes");
-
-let isRunning = true;
-
-let counterInterval = setInterval(function() {
-  isRunning = true;
-  changeTimer(1);
-}, 1000);
-
+// Group listener to handle all button clicks
 buttonDiv.addEventListener("click", function(e) {
   if (e.target.dataset.name === "-") {
     changeTimer(-1);
@@ -31,11 +31,15 @@ buttonDiv.addEventListener("click", function(e) {
     changeTimer(1);
   }
   if (e.target.dataset.name === "heart") {
+    //   Check if an object with that number already exists or make a new one if it doesn't
     obj = findObj();
     if (document.getElementById(obj.num)) {
       const li = document.getElementById(obj.num);
+      //   Make it exists on the page replace its inner text with the updated one (which has the new like count)
       li.innerText = `Number ${obj.num} has ${obj.likes} likes`;
-    } else {
+    }
+    // Make a new list item on the page
+    else {
       likeList.innerHTML += `<li id='${obj.num}'>Number ${obj.num} has ${
         obj.likes
       } likes</li>`;
@@ -46,18 +50,21 @@ buttonDiv.addEventListener("click", function(e) {
   }
 });
 
+// Function to increase or decrease timer clock given a num
 function changeTimer(num) {
   counter.innerText = `${parseInt(counter.innerText) + num}`;
 }
 
+// Looks to see if the object exists
 function findObj() {
+  // If it exists increment it and return it
   for (let obj of heartArray) {
     if (obj.num == counter.innerText) {
       obj.likes++;
       return obj;
     }
   }
-
+  // If it doesn't exist make a new one and return it
   const newObj = {
     num: counter.innerText,
     likes: 1
@@ -67,10 +74,13 @@ function findObj() {
 }
 
 function toggleTimer() {
+  // If the timer is running, stop it
   if (isRunning === true) {
     clearInterval(counterInterval);
     isRunning = false;
-  } else {
+  }
+  // If its not running, start it
+  else {
     counterInterval = setInterval(function() {
       isRunning = true;
       changeTimer(1);
